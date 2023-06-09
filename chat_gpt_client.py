bff57da82951701a7b5e0bd9672323a4b64e2d7d
Adam <fitzadd@gmail.com>
2023-06-09 14:57:58 +0100

    Adding chat GPT client code

---
+++ chat_gpt_client.py
@@ -0,0 +1,23 @@
+import os
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAPI_KEY")
+
+question = input("Ask your question here: ")
+
+answer = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo-0301",
+    messages=[
+        {"role": "system", "content": "You are a chatbot"},
+        {"role": "user", "content": f"{ question }" },
+    ]
+)
+
+result = ''
+for choice in response.choices:
+    result += choice.message.content
+
+print(result) 
